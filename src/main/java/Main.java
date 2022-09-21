0fd06d627a6136e4b3caea1451d0cf2dcb280054
hmlimkokhoe <lim.kokhoe93@googlemail.com>
2022-09-21 14:11:09 +0200

    Write SVG string to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,25 @@
 import edu.hm.hafner.renderer.EChartsSvgRenderer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Demo project showing the trireme-java dependency.
  */
 public class Main {
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    private static final String FILE_PATH = "target/output.svg";
+
+    public static void main(String[] args) throws IOException {
         JsonReader jsonReader = new JsonReader();
         String configOptions = jsonReader.getJsonAsString("/pie-chart-config.json");
         String exportOptions = jsonReader.getJsonAsString("/export-config.json");
 
         EChartsSvgRenderer echartsSvgRenderer = new EChartsSvgRenderer();
         String result = echartsSvgRenderer.createSvgString(configOptions, exportOptions);
-        LOG.info(result);
+        Path path = Paths.get(FILE_PATH);
+        Files.write(path, result.getBytes());
     }
 }
